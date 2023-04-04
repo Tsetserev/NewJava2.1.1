dd92cf7a43eaec078cd0cb672790f6633dd128b5
Aleksandr <tsetserev123@gmail.com>
2023-04-04 10:58:18 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int price = 15600;
         int condition = 20;
-        int finalBonus = price/condition;
+        int finalBonus = price / condition;
 
         System.out.println((finalBonus) + " Миль(и) Вы Получите");
     }
